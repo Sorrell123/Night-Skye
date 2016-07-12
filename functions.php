1312871bd4ab0ef01e638ee642f25aae89f8029d
Sorrell123 <multibrunio@gmail.com>
2016-07-12 20:33:33 +0200

    Add menu and thumbnail functions

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+//Dodaje możliwość wstawienia zdjęcia jako "nagłówek" postu
+add_theme_support("post-thumbnails"); 
+
+//Dodaje opcje wstawianie nowych przycisków do menu
+add_theme_support("menus");
+
+?>
