2d811db9629463cf623bd838dfd80b9bf607f98e
Sorrell123 <multibrunio@gmail.com>
2016-07-12 20:35:55 +0200

    Create footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+  <div id="footer">
+	  <a href="http://validator.w3.org/check?uri=referer">Valid XHTML</a> | <a href="http://fotogrph.com/">Images</a> | <a href="http://www.heartinternet.co.uk/web-hosting/">Web Hosting</a> | website template by <a href="#"><?php the_author_posts_link(); ?></a>
+  </div><!--close footer-->  
+  
+</body>
+</html>
