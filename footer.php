48de6299786bf06eee096ee69a08b91a5fa28ec5
Sorrell123 <multibrunio@gmail.com>
2016-07-12 23:10:36 +0200

    Working on menu and slider

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
   <div id="footer">
-	  <a href="http://validator.w3.org/check?uri=referer">Valid XHTML</a> | <a href="http://fotogrph.com/">Images</a> | <a href="http://www.heartinternet.co.uk/web-hosting/">Web Hosting</a> | website template by <a href="#"><?php the_author_posts_link(); ?></a>
+	  <a href="http://validator.w3.org/check?uri=referer">Valid XHTML</a> | <a href="http://fotogrph.com/">Images</a> | <a href="http://www.heartinternet.co.uk/web-hosting/">Web Hosting</a> | website template by <a href="<?php bloginfo("url"); ?>">Sorrell</a>
   </div><!--close footer-->  
   
 </body>
--- header.php
+++ header.php
@@ -7,9 +7,9 @@
   <meta name="keywords" content="enter your keywords here" />
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" type="text/css" href="<?php bloginfo("stylesheet_url"); ?>" />
-  <script type="text/javascript" src="js/jquery.min.js"></script>
-  <script type="text/javascript" src="js/jquery.easing.min.js"></script>
-  <script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+  <script type="text/javascript" src="<?php bloginfo("template_url"); ?>/js/jquery.min.js"></script>
+  <script type="text/javascript" src="<?php bloginfo("template_url"); ?>/js/jquery.easing.min.js"></script>
+  <script type="text/javascript" src="<?php bloginfo("template_url"); ?>/js/jquery.nivo.slider.pack.js"></script>
   <script type="text/javascript">
     $(window).load(function() {
         $('#slider').nivoSlider();
@@ -25,7 +25,46 @@
     <div id="welcome">
       <h1><a href="<?php echo get_option("home"); ?>"><?php bloginfo("name"); ?></a></h1>
     </div><!--close welcome-->
-      <div id="menu_items">
-      <?php pw_nav_menu(); ?>
+    <div id="menu_items">
+      <!-- tablica która pozwala dobrać odpowiedni klucz z biblioteki do odpowiedniego elementu w menu -->
+      <?php
+        wp_nav_menu(array(
+          'menu_id' => "menu",
+        ));
+      ?>
     </div><!--close menu-->
-  </div><!--close menubar-->  
+  </div><!--close menubar--> 
+
+  <div id="site_content">   
+
+    <div id="banner_image">
+      <div id="slider-wrapper">        
+        <div id="slider" class="nivoSlider">
+          <!--
+            Tu petla po wszystkich obrazkach w katalogu home-images w wp_uploads_dir.
+          -->
+          <img src="<?php bloginfo("template_url"); ?>/images/home_1.jpg" alt="" />
+          <img src="<?php bloginfo("template_url"); ?>/images/home_2.jpg" alt="" />
+          <?php
+
+          $args = array(
+              'post_type' => 'attachment',
+              'numberposts' => -1,
+              'post_status' => null,
+              'post_parent' => null, // any parent
+              'post_mime_type' => 'image'
+              ); 
+          $attachments = get_posts($args);
+          if ($attachments) {
+              foreach ($attachments as $post) {
+                  setup_postdata($post); // pewnie ustawia że funkcje poniżej działają działa
+                  if(get_the_title() == "home-image"){
+                    the_attachment_link($post->ID, false);
+                  }
+              }
+          }
+
+          ?>
+        </div><!--close slider-->
+      </div><!--close slider_wrapper-->
+    </div><!--close banner_image--> 
